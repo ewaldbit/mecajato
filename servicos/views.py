96c44b294dad0e0fb23775cd1261b0add7f908e6
Ewald Bitencourt <ewaldbit@gmail.com>
2024-12-23 15:43:57 -0300

    listagem de serviços

--- servicos/views.py
+++ servicos/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import FormServico
 from django.http import HttpResponse
+from .models import Servicos
 
 def novo_servico(request):
     if request.method == "GET":
@@ -16,7 +17,12 @@ def novo_servico(request):
             return render(request, 'novo_servico.html', {'form', form} )
         
 def listar_servico(request):
-    
+    if request.method == "GET":
+        servicos = Servicos.objects.all()
+        return render(request, 'listar_servico.html', {'servicos': servicos})
 
+def servico(request, identificador):
+    servico = get_object_or_404(Servicos, identificador=identificador)
+    return render(request, 'servico.html', {'servico': servico})
 
     
